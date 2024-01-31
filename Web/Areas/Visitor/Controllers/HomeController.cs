26f0a50602937e32475619523b77a5e6f96a07fb
DaCrunkness <jerell.smith.09@gmail.com>
2024-01-30 20:25:01 -0600

    display products and details

--- Web/Areas/Visitor/Controllers/HomeController.cs
+++ Web/Areas/Visitor/Controllers/HomeController.cs
@@ -1,14 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Models.DataModels;
 
 namespace Web.Areas.Visitor.Controllers
 {
     [Area("Visitor")]
-
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            Product product= _unitOfWork.Product.Get(u=>u.Id == productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
